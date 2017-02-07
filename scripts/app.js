228acf3abfa072e43b8ada5a4afde3ce2c63f04a
holbrook <wanghaikuo@gmail.com>
2017-02-07 16:37:12 +0800

    Generate Pelican site

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-$("div#sidebar").append(
-    $("div#TOC").html()
-    );
-$("div#TOC").empty();
-
-$("div#sidebar").append(
-    $("div#table-of-contents").html()
-    );
-$("div#table-of-contents").empty();
